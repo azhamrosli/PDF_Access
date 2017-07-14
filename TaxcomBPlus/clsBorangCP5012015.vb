4f48bf96b684bdb88876adbcb7e639655eac5c70
azhamrosli <arsoftwaremalaysia@gmail.com>
2017-07-14 12:32:04 +0800

    1st commit

---
+++ TaxcomBPlus/clsBorangCP5012015.vb
@@ -0,0 +1,205 @@
+Imports iTextSharp.text.pdf
+Imports System.Data.OleDb
+Imports System.IO
+Imports System
+
+Public Class clsBorangCP5012015
+    Private Const pdfSubFormName = "topmostSubform[0]."
+
+    Dim pdfForm As New clsPDFMaker
+    Dim pdfFormFields As AcroFields
+    Dim datHandler As New clsDataHandler("")
+
+#Region "CStor"
+
+    Public Sub New()
+
+        datHandler = New clsDataHandler(pdfForm.GetFormType)
+        pdfFormFields = pdfForm.GetStamper.AcroFields
+        Page1()
+        pdfForm.OpenFile()
+        pdfForm.CloseStamper()
+    End Sub
+#End Region
+
+    Private Sub Page1()
+
+        Dim pdfFieldPath As String = ""
+        Dim strTempString As String = ""
+        Dim dr As OleDbDataReader = Nothing
+
+
+        Try
+            ' === Part Slip === '
+            pdfFieldPath = pdfSubFormName & "Page1[0]."
+            dr = datHandler.GetDataReader("Select * from taxp_profile where (tp_ref_no1 + tp_ref_no2 + tp_ref_no3)= '" & pdfForm.GetRefNo & "'")
+            If dr.Read() Then
+                If Not IsDBNull(dr("tp_ref_no_prefix")) And Not IsDBNull(dr("tp_ref_no1")) And Not IsDBNull(dr("tp_ref_no2")) And Not IsDBNull(dr("tp_ref_no3")) Then
+                    If Not String.IsNullOrEmpty(dr("tp_ref_no_prefix").ToString & dr("tp_ref_no1").ToString & dr("tp_ref_no2").ToString & dr("tp_ref_no3").ToString) Then
+                        pdfFormFields.SetField(pdfFieldPath & "Slip1", dr("tp_ref_no_prefix") & dr("tp_ref_no1") & dr("tp_ref_no2") & dr("tp_ref_no3"))
+                    End If
+                End If
+
+                'danny ------------------------------------------
+                If Not IsDBNull(dr("TP_CURR_ADD_LINE1")) Then
+                    If Not IsDBNull(dr("TP_CURR_ADD_LINE2")) Then
+                        If Not IsDBNull(dr("TP_CURR_ADD_LINE3")) Then
+                            pdfFormFields.SetField(pdfFieldPath & "Address1", dr("TP_CURR_ADD_LINE1").ToString)
+                            pdfFormFields.SetField(pdfFieldPath & "Address2", dr("TP_CURR_ADD_LINE2").ToString)
+                            pdfFormFields.SetField(pdfFieldPath & "Address3", dr("TP_CURR_ADD_LINE3").ToString)
+                            pdfFormFields.SetField(pdfFieldPath & "Address4", dr("TP_CURR_POSTCODE").ToString + dr("TP_CURR_CITY").ToString)
+                            pdfFormFields.SetField(pdfFieldPath & "Address5", dr("TP_CURR_STATE").ToString)
+                        Else
+                            pdfFormFields.SetField(pdfFieldPath & "Address1", dr("TP_CURR_ADD_LINE1").ToString)
+                            pdfFormFields.SetField(pdfFieldPath & "Address2", dr("TP_CURR_ADD_LINE2").ToString)
+                            pdfFormFields.SetField(pdfFieldPath & "Address3", dr("TP_CURR_POSTCODE").ToString + dr("TP_CURR_CITY").ToString)
+                            pdfFormFields.SetField(pdfFieldPath & "Address4", dr("TP_CURR_STATE").ToString)
+                            pdfFormFields.SetField(pdfFieldPath & "Address5", "")
+                        End If
+                    Else
+                        pdfFormFields.SetField(pdfFieldPath & "Address1", dr("TP_CURR_ADD_LINE1").ToString)
+                        pdfFormFields.SetField(pdfFieldPath & "Address2", dr("TP_CURR_POSTCODE").ToString + dr("TP_CURR_CITY").ToString)
+                        pdfFormFields.SetField(pdfFieldPath & "Address3", dr("TP_CURR_STATE").ToString)
+                        pdfFormFields.SetField(pdfFieldPath & "Address4", "")
+                        pdfFormFields.SetField(pdfFieldPath & "Address5", "")
+                    End If
+                Else
+                    pdfFormFields.SetField(pdfFieldPath & "Address1", "")
+                    pdfFormFields.SetField(pdfFieldPath & "Address2", "")
+                    pdfFormFields.SetField(pdfFieldPath & "Address3", "")
+                    pdfFormFields.SetField(pdfFieldPath & "Address4", "")
+                    pdfFormFields.SetField(pdfFieldPath & "Address5", "")
+
+                End If
+                'end ---------------------------------------------
+                If Not IsDBNull(dr("TP_NAME")) Then
+                    If Not String.IsNullOrEmpty(dr("TP_NAME").ToString) Then
+                        pdfFormFields.SetField(pdfFieldPath & "Name", dr("TP_NAME").ToString)
+                    End If
+                End If
+
+                'If Not IsDBNull(dr("TP_CURR_ADD_LINE1")) Then
+                '    If Not String.IsNullOrEmpty(dr("TP_CURR_ADD_LINE1").ToString) Then
+                '        strTempString = strTempString + dr("TP_CURR_ADD_LINE1").ToString
+                '    End If
+                'End If
+
+                'If Not IsDBNull(dr("TP_CURR_ADD_LINE2")) Then
+                '    If Not String.IsNullOrEmpty(dr("TP_CURR_ADD_LINE2").ToString) Then
+                '        If Right(Trim(dr("TP_CURR_ADD_LINE2")), 1) = "," Then
+                '            strTempString = strTempString + " " + dr("TP_CURR_ADD_LINE2").ToString
+                '        Else
+                '            strTempString = strTempString + ", " + dr("TP_CURR_ADD_LINE2").ToString
+                '        End If
+                '    End If
+                'End If
+
+                'If Not IsDBNull(dr("TP_CURR_ADD_LINE3")) Then
+                '    If Not String.IsNullOrEmpty(dr("TP_CURR_ADD_LINE3").ToString) Then
+                '        If Right(Trim(dr("TP_CURR_ADD_LINE3")), 1) = "," Then
+                '            strTempString = strTempString + " " + dr("TP_CURR_ADD_LINE3").ToString
+                '        Else
+                '            strTempString = strTempString + ", " + dr("TP_CURR_ADD_LINE3").ToString
+                '        End If
+                '    End If
+                'End If
+                'If Not IsDBNull(dr("TP_CURR_POSTCODE")) Then
+                '    If Not String.IsNullOrEmpty(dr("TP_CURR_POSTCODE").ToString) Then
+                '        strTempString = strTempString + Environment.NewLine + dr("TP_CURR_POSTCODE").ToString
+                '    End If
+                'End If
+                'If Not IsDBNull(dr("TP_CURR_CITY")) Then
+                '    If Not String.IsNullOrEmpty(dr("TP_CURR_CITY").ToString) Then
+                '        strTempString = strTempString + " " + dr("TP_CURR_CITY").ToString + Environment.NewLine
+                '    End If
+                'End If
+                'If Not IsDBNull(dr("TP_CURR_STATE")) Then
+                '    If Not String.IsNullOrEmpty(dr("TP_CURR_STATE").ToString) Then
+                '        strTempString = strTempString + dr("TP_CURR_STATE").ToString
+                '    End If
+                'End If
+                'If Not String.IsNullOrEmpty(strTempString) Then
+                '    pdfFormFields.SetField(pdfFieldPath & "Slip3", strTempString.ToString.ToUpper)
+                'End If
+
+                strTempString = ""
+                If Not IsDBNull(dr("TP_IC_NEW_1")) Then
+                    If Not String.IsNullOrEmpty(dr("TP_IC_NEW_1").ToString) Then
+                        strTempString = strTempString + dr("TP_IC_NEW_1").ToString
+                    End If
+                End If
+                If Not IsDBNull(dr("TP_IC_NEW_2")) Then
+                    If Not String.IsNullOrEmpty(dr("TP_IC_NEW_2").ToString) Then
+                        strTempString = strTempString + dr("TP_IC_NEW_2").ToString
+                    End If
+                End If
+                If Not IsDBNull(dr("TP_IC_NEW_3")) Then
+                    If Not String.IsNullOrEmpty(dr("TP_IC_NEW_3").ToString) Then
+                        strTempString = strTempString + dr("TP_IC_NEW_3").ToString
+                    End If
+                End If
+                If Not String.IsNullOrEmpty(strTempString) Then
+                    pdfFormFields.SetField(pdfFieldPath & "IC", strTempString)
+                End If
+            End If
+            dr.Close()
+
+            dr = datHandler.GetDataReader("Select TC_BALANCE_TAX_PAYABLE from tax_computation where tc_ref_no= '" & pdfForm.GetRefNo & "' and tc_ya= '" & pdfForm.GetYA & "'")
+            If dr.Read() Then
+                If Not IsDBNull("TC_BALANCE_TAX_PAYABLE") Then
+                    pdfFormFields.SetField(pdfFieldPath & "Amount", FormatFloatingAmount(dr("TC_BALANCE_TAX_PAYABLE").ToString, True))
+                End If
+            End If
+            dr.Close()
+
+            pdfFormFields.SetField(pdfFieldPath & "Slip5", "")
+            pdfFormFields.SetField(pdfFieldPath & "Slip6", "")
+            pdfFormFields.SetField(pdfFieldPath & "Slip7", "")
+            pdfFormFields.SetField(pdfFieldPath & "Slip8", "")
+
+
+            'Nama dan Ruj
+            dr = datHandler.GetDataReader("select tp_name from taxp_profile where " _
+                        & "(tp_ref_no1 + tp_ref_no2 + tp_ref_no3)= '" & pdfForm.GetRefNo & "'")
+            If dr.Read Then
+                If Not IsDBNull(dr("tp_name")) Then
+                    If Not String.IsNullOrEmpty(dr("tp_name").ToString) Then
+                        pdfFormFields.SetField(pdfFieldPath & "Nama11", dr("tp_name").ToString.ToUpper)
+                    End If
+                End If
+                If Not String.IsNullOrEmpty(pdfForm.GetRefNo) Then
+                    pdfFormFields.SetField(pdfFieldPath & "Ruj11", pdfForm.GetRefNo)
+                End If
+            End If
+            dr.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.ToString, MsgBoxStyle.Critical)
+        End Try
+
+    End Sub
+
+    Protected Function FormatFloatingAmount(ByVal strTemp As String, ByVal intFloating As Boolean) As String
+
+        If intFloating = True Then
+            If Not strTemp = "" Then
+                If CDbl(strTemp) > 0 Then
+                    strTemp = strTemp.ToString.Replace(",", "").Replace(".", "")
+                Else
+                    strTemp = "000"
+                End If
+            End If
+        Else
+            If Not strTemp = "" Then
+                If CDbl(strTemp) > 0 Then
+                    strTemp = Math.Ceiling(CDbl(strTemp)).ToString.Replace(",", "")
+                Else
+                    strTemp = "0"
+                End If
+            End If
+        End If
+        Return strTemp
+
+    End Function
+
+End Class
---
+++ TaxcomBPlus/clsDataHandler.vb
@@ -0,0 +1,147 @@
+Imports System.Data.OleDb
+Imports System.IO
+
+Public Class clsDataHandler
+    Dim strConn As String = ""
+
+    Public Sub New(ByVal strAppType As String)
+
+        If Not strAppType = "" Then
+            InitialiseConnection(strAppType)
+        End If
+    End Sub
+
+    Private Sub InitialiseConnection(ByVal strAppType As String)
+
+        Dim strFullPath As String = Nothing
+        Dim strContents As String = Nothing
+        Dim tmp As String = Nothing
+
+        Try
+            Dim objReader As StreamReader
+            If strAppType = "B" Or strAppType = "BE" Or strAppType = "M" Then
+                strFullPath = Application.StartupPath & "..\TaxcomB.ini" 'after make exe run this
+            ElseIf strAppType = "P" Or strAppType = "CP30" Then
+                strFullPath = Application.StartupPath & "..\TaxcomP.ini" 'after make exe run this
+            End If
+
+            objReader = Nothing
+            strContents = Nothing
+
+            objReader = New StreamReader(strFullPath)
+            strContents = objReader.ReadLine
+
+            objReader.Close()
+            strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & strContents
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+
+        End Try
+    End Sub
+
+    Public Function GetDataReader(ByVal strQuery As String) As OleDbDataReader
+        Dim dr As OleDbDataReader
+        Dim cmd As New OleDbCommand
+        With cmd
+            ' Create a Connection object
+            .Connection = New OleDbConnection(strConn)
+            .Connection.Open()
+            .CommandText = strQuery
+            dr = .ExecuteReader(CommandBehavior.CloseConnection)
+        End With
+        'If Not dr.HasRows Or dr.RecordsAffected <= 0 Then
+        '    dr = Nothing
+        'End If
+        Return dr
+    End Function
+
+    Public Function GetDataReader1(ByVal strQuery As String) As OleDbDataReader
+        Dim dr As OleDbDataReader
+        Dim cmd As New OleDbCommand
+        With cmd
+            ' Create a Connection object
+            InitialiseConnection("B")
+            .Connection = New OleDbConnection(strConn)
+            .Connection.Open()
+            .CommandText = strQuery
+            dr = .ExecuteReader(CommandBehavior.CloseConnection)
+        End With
+        Return dr
+    End Function
+
+    Public Function GetData(ByVal strQuery As String) As DataSet
+
+        Dim ds As New DataSet
+        Dim dataConnection As New OleDbConnection
+        dataConnection.ConnectionString = strConn
+        Try
+            Dim cmd As New OleDbCommand(strQuery, dataConnection)
+            'If prmOleDb IsNot Nothing Then
+            'For Each prmOle As OleDbParameter In prmOleDb
+            'If prmOle IsNot Nothing Then cmd.Parameters.Add(prmOle)
+            '    Next
+            'End If
+            Dim da As New OleDbDataAdapter(cmd)
+            da.Fill(ds)
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        Finally
+            If dataConnection.State = ConnectionState.Open Then dataConnection.Close()
+        End Try
+
+        Return ds
+    End Function
+
+    Public Function GetData(ByVal strQuery As String, ByVal ParamArray prmOleDb As IDataParameter()) As DataSet
+
+        Dim ds As New DataSet
+        Dim dataConnection As New OleDbConnection
+        dataConnection.ConnectionString = strConn
+        Try
+            Dim cmd As New OleDbCommand(strQuery, dataConnection)
+
+            If prmOleDb IsNot Nothing Then
+                For Each prmOle As OleDbParameter In prmOleDb
+                    If prmOle IsNot Nothing Then cmd.Parameters.Add(prmOle)
+                Next
+            End If
+            Dim da As New OleDbDataAdapter(cmd)
+            da.Fill(ds)
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        Finally
+            If dataConnection.State = ConnectionState.Open Then dataConnection.Close()
+        End Try
+
+        Return ds
+    End Function
+
+    Public Function Execute(ByVal strSQL As String) As Integer
+        Dim objConn As New OleDbConnection(strConn)
+        Dim cmd As OleDbCommand
+        Dim intAffectedRow As Integer
+
+        Try
+            cmd = New OleDbCommand
+            cmd.Connection = objConn
+            objConn.Open()
+            cmd.CommandText = strSQL
+            cmd.ExecuteNonQuery()
+
+        Catch ex As Exception
+            MsgBox(ex.ToString, MsgBoxStyle.Critical)
+        Finally
+            objConn.Close()
+        End Try
+
+        Return intAffectedRow
+    End Function
+
+    Public ReadOnly Property OledbConnection()
+        Get
+            Return strConn
+        End Get
+    End Property
+End Class
